cbceeb398b6c984d267759f75379faf261afc227
Marsel Gabbasov <marsel13_98@mail.ru>
2022-07-16 13:54:02 +0500

    sdds

--- src/js/components/project.js
+++ src/js/components/project.js
@@ -1,8 +1,8 @@
 function progressSlider() {
     var swiper = new Swiper('.progress__slider .swiper-container', {
-        slidesPerView: 1,
+        slidesPerView: 3,
         spaceBetween: 12,
-        centeredSlides: true,
+        // centeredSlides: true,
         loop: true,
         navigation: {
             nextEl: '.progress__slider .swiper-button-next',
@@ -13,6 +13,14 @@ function progressSlider() {
             type: 'bullets',
             clickable: true,
         },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            576: {
+                slidesPerView: 3,
+            },
+        }
     })
 }
 
